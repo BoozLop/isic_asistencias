8aa886b2f46eb11abe08ed1505f547f423765735
BoozLop <delgadobenito596@gmail.com>
2020-04-01 19:21:08 -0600

    Practica # 1 – Puesta en marcha del sistema, creacion y cambio de tema creado

---
+++ betali_v2_asistencias-master/mLogin/login.php
@@ -0,0 +1,31 @@
+<div class="login-box" >
+    <div class="login-box-body bordeLogin">
+
+        <p class="tituloLogin">Sistema Control de Accesos</p>
+        
+        <form action="verificar_login.php" method="post" id="frmLogin" style="border-color:#40739e">
+            <div class="form-group has-feedback logoActivo">
+                <label class="lblTitulo">Usuario:</label>
+            <input type="usuario" id="loginUsuario" class="form-control" placeholder="Escribe el nombre de usuario" autofocus required>
+            </div>
+            <div class="form-group has-feedback">
+                <label class="lblTitulo">Contraseña:</label>
+            <input type="password" id="loginContra" class="form-control" placeholder="Escribe la contraseña" required>
+           
+            </div>
+
+                <div class="col text-center">
+                    <button type="submit" class="btn btn-outline-dark btn-block active" id="btnIngresar" disabled>
+                        <i class="fas fa-lock" id="icoLogin"></i>
+                        Ingresar al sistema
+                    </button>
+                </div><br>
+                <div class="col text-center">
+                    <button class="btn btn-outline-info btn-block" data-toggle="modal" data-target="#modalContra">
+                        <i class="fas fa-key" id="icoContra"></i>
+                        Cambiar contraseña
+                    </button>
+                </div>
+        </form>
+    </div>
+</div>
