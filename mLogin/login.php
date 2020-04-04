70aa0818c8163d9f6bbbd433c5632c5f755e1b54
Dario Lopez <55515973+BoozLop@users.noreply.github.com>
2020-04-03 18:55:47 -0600

    Practica#2-Cambio de contraseña antes de ingresar

---
+++ mLogin/login.php
@@ -0,0 +1,29 @@
+<div class="login-box" >
+    <div class="login-box-body bordeLogin">
+        <p class="tituloLogin">Sistema Control de Accesos</p>
+        
+        <form action="verificar_login.php" method="post" id="frmLogin" style="border-color:#40739e">
+            <div class="form-group has-feedback logoActivo">
+                <label class="lblTitulo">Usuario:</label>
+            <input type="usuario" id="loginUsuario" class="form-control" placeholder="Escribe el nombre de usuario" autofocus required>
+            </div>
+            <div class="form-group has-feedback">
+                <label class="lblTitulo">Contraseña:</label>
+            <input type="password" id="loginContra" class="form-control" placeholder="Escribe la contraseña" required>
+           
+            </div>
+                <div class="col text-center">
+                    <button   type="submit" class="btn btn-outline-dark active" id="btnIngresar" disabled>
+                        <i class="fas fa-lock" id="icoLogin"></i>
+                        Ingresar al sistema
+                    </button>
+                </div>
+                <br>
+                <div class="col text-center">
+                <input type="checkbox" data-toggle="toggle" data-size="sm" data-onstyle="outline-success" data-offstyle="outline-danger" data-on="SI" data-off="NO" id="camcon">
+                        Actualizar contraseña
+                    </button>
+                </div>
+        </form>
+    </div>
+</div>
